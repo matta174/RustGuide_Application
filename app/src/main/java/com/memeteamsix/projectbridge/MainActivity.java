d43142ebe45a1713a11f227d36804d0473471ba3
Matthew <matthewc174@gmail.com>
2016-04-19 00:40:10 -0400

    Fixed some fab issues

--- app/src/main/java/com/memeteamsix/projectbridge/MainActivity.java
+++ app/src/main/java/com/memeteamsix/projectbridge/MainActivity.java
@@ -44,6 +44,19 @@ public class MainActivity extends AppCompatActivity
         Button fab2 = (Button) findViewById(R.id.database_btn);
         Button fab3 = (Button) findViewById(R.id.tools_btn);
         Button fab4 = (Button) findViewById(R.id.links_btn);
+
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+
     }
 
     View.OnClickListener a = new View.OnClickListener() {
@@ -88,14 +101,7 @@ public class MainActivity extends AppCompatActivity
         */
 
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
 
         //LoadDB tester please ignore
         try{
