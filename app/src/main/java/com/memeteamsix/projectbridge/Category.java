03dc8e166110d3efd18788224a18962f6870a949
Josh Nicholson <joshwnicholson@gmail.com>
2016-04-18 23:12:42 -0400

    Uploaded object class files for categories

---
+++ app/src/main/java/com/memeteamsix/projectbridge/Category.java
@@ -0,0 +1,40 @@
+package com.memeteamsix.projectbridge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by jwnicholson on 4/18/2016.
+ */
+public class Category {
+    private String name = "";
+    private List<Subcategory> subs = new ArrayList<Subcategory>();
+    private String picName;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Subcategory> getSubs() {
+        return subs;
+    }
+
+    public void setSubs(List<Subcategory> subs) {
+        this.subs = subs;
+    }
+
+    public String getPicName() {
+        return picName;
+    }
+
+    public void setPicName(String picName) {
+        this.picName = picName;
+    }
+
+
+}
