6d997904652f3f636e511e25947c17d0b23683b4
Matthew <matthewc174@gmail.com>
2016-04-19 23:29:30 -0400

    Did guides stuff, and added it to XML finally bringing us the many suc

--- app/src/main/java/com/memeteamsix/projectbridge/Guides.java
+++ app/src/main/java/com/memeteamsix/projectbridge/Guides.java
@@ -1,7 +1,67 @@
 package com.memeteamsix.projectbridge;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
 /**
  * Created by Matthew on 4/18/2016.
  */
-public class Guides {
-}
+public class Guides extends AppCompatActivity
+{
+    private Button rustWebGuide;
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        rustWebGuide = (Button) findViewById(R.id.rustGuide_btn);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guides);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        View.OnClickListener a = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("http://rust.wikia.com/wiki/Basic_Survival_Guide"));
+                startActivity(intent);
+
+            }
+        };
+
+        rustWebGuide.setOnClickListener(a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}//end main
